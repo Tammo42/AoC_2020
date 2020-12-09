fc86cfdebb0c2601b72bac8a3ccc76135727efbe
Tammo <tammo.heilemann+github@gmail.com>
2020-12-09 14:10:33 +0100

    Setup puzzle for day 07

---
+++ 07.py
@@ -0,0 +1,28 @@
+import os
+from adventofcode import AdventDay
+from functools import reduce
+
+class Day07_01(AdventDay):
+    def __init__(self, input_path : str) -> None:
+        in_file = open(input_path, "r")
+        pass
+
+    def do(self) -> None:
+        pass
+    pass
+
+class Day07_02(AdventDay):
+    def __init__(self, input_path : str) -> None:
+        in_file = open(input_path, "r")
+        pass
+
+    def do(self) -> None:
+        pass
+    pass
+
+input_path = os.path.dirname(os.path.realpath(__file__)) + "/07-01_input.txt"
+mission = Day07_01(input_path)
+mission.do()
+print("")
+mission = Day07_02(input_path)
+mission.do()
