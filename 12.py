05c85eb68b8c27f7112f70809cb07de13195ff48
Tammo <Tammo42@users.noreply.github.com>
2020-12-27 14:42:58 +0100

    Christmas was not saved but day 12 is solved now

--- 12.py
+++ 12.py
@@ -2,6 +2,16 @@ from adventofcode import AdventDay
 from enum import Enum
 import os
 
+def manhatten_distance(position):
+    (x,y) = position
+    if x < 0:
+        x = -x
+        pass
+    if y < 0:
+        y = -y
+        pass
+    return x + y
+
 class Direction(Enum):
     NORTH = (0,1)
     EAST = (1,0)
@@ -10,7 +20,7 @@ class Direction(Enum):
 
     def __mul__(self, other):
         if isinstance(other, int):
-            (x,y) = self.value
+            x, y = self.value
             return (x * other, y * other)
         else:
             raise Exception("Can only multiply int with Direction")
@@ -59,15 +69,6 @@ class Ship:
         pass
 
     def get_manhatten_distance(self):
-        def manhatten_distance(position):
-            (x,y) = position
-            if x < 0:
-                x = -x
-                pass
-            if y < 0:
-                y = -y
-                pass
-            return x + y
         return manhatten_distance(self.position)
     pass
 
@@ -113,14 +114,73 @@ class Day12_01(AdventDay):
         pass
     pass
 
+class Marker:
+    def __init__(self):
+        self.ship = (0,0)
+        self.waypoint = (10,1)
+        pass
+
+    def move_waypoint(self, vector):
+        self.waypoint = (self.waypoint[0]+vector[0], self.waypoint[1]+vector[1])
+        pass
+
+    def turn_waypoint(self):
+        self.waypoint = (self.ship[0]+self.waypoint[1]-self.ship[1],\
+                         self.ship[1]-(self.waypoint[0]-self.ship[0]))
+        pass
+
+    def move_ship(self, distance = 1):
+        tmp_waypoint = (self.waypoint[0]-self.ship[0], self.waypoint[1]-self.ship[1])
+        self.ship = (self.ship[0]+tmp_waypoint[0]*distance,self.ship[1]+tmp_waypoint[1]*distance)
+        self.move_waypoint(vector=(tmp_waypoint[0]*distance,tmp_waypoint[1]*distance))
+        pass
+    pass
+
 class Day12_02(AdventDay):
     def __init__(self, input_path : str) -> None:
+        self.navigation_list = list()
+        self.marker = Marker()
         for line in open(input_path, "r"):
             line = line.strip()
+            if not line == "":
+                self.navigation_list.append((line[:1],int(line[1:])))
+                pass
             pass
         pass
 
     def do(self) -> None:
+        for i, n in self.navigation_list:
+            if i == "N":
+                self.marker.move_waypoint((0,n))
+                pass
+            elif i == "S":
+                self.marker.move_waypoint((0,-n))
+                pass
+            elif i == "E":
+                self.marker.move_waypoint((n,0))
+                pass
+            elif i == "W":
+                self.marker.move_waypoint((-n,0))
+                pass
+            elif i == "L":
+                for _ in range(int(n/90)):
+                    for _ in range(3):
+                        self.marker.turn_waypoint()
+                        pass
+                    pass
+                pass
+            elif i == "R":
+                for _ in range(int(n/90)):
+                    self.marker.turn_waypoint()
+                    pass
+                pass
+            elif i in ["F"]:
+                self.marker.move_ship(n)
+                pass
+            else:
+                raise Exception("Line did not contain navigational instructions.")
+            pass
+        print(manhatten_distance(self.marker.ship))
         pass
     pass
 
